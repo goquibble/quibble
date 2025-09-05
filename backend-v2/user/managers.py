7712d820fdac1c6f38550475ff7ce253eb20c27f
stabldev <114811070+stabldev@users.noreply.github.com>
2025-09-05 23:09:39 +0530

    feat: add custom user model

---
+++ backend-v2/user/managers.py
@@ -0,0 +1,32 @@
+from typing import Any
+from django.contrib.auth.models import BaseUserManager
+
+
+class CustomUserManager(BaseUserManager[Any]):
+    def _create_user(self, email: str, password: str | None, **extra_fields: Any):
+        if not email:
+            raise ValueError("Email is required")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_user(self, email: str, password: str | None, **extra_fields: Any):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email: str, password: str | None, **extra_fields: Any):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True")
+
+        return self._create_user(email, password, **extra_fields)
--- backend-v2/user/models.py
+++ backend-v2/user/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from user.managers import CustomUserManager
+
+
+class CustomUser(AbstractUser):
+    username = models.CharField(max_length=50, unique=True, blank=True)
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()  # pyright: ignore[reportAssignmentType]
