6ded786f48f723e6e53ef13e7eeca3a2f0a178f9
stabldev <114811070+stabldev@users.noreply.github.com>
2025-09-16 19:27:38 +0530

    feat: add custom _allauth session-view

--- backend-v2/config/urls.py
+++ backend-v2/config/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 from api.urls import api_v1
+from api.views.auth import CustomSessionView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -9,4 +10,5 @@ urlpatterns = [
     # django-allauth urls and headless apis
     path("accounts/", include("allauth.urls")),
     path("_allauth/", include("allauth.headless.urls")),
+    path("_allauth/browser/v1/auth/session", CustomSessionView.as_view()),
 ]
