6c015efb0288336faeb8fd5531b31140d7650813
stabldev <114811070+stabldev@users.noreply.github.com>
2025-09-23 08:07:51 +0530

    fix: custom session view circular imports

--- backend-v2/config/urls.py
+++ backend-v2/config/urls.py
@@ -1,5 +1,4 @@
 from typing import Callable, cast
-from allauth.headless.constants import Client
 from django.contrib import admin
 from django.http import HttpRequest, HttpResponse
 from django.urls import include, path
@@ -9,17 +8,21 @@ from django.conf.urls.static import static
 from api.urls import api_v1
 from api.views.auth import CustomSessionView
 
-custom_session_view = cast(
-    Callable[[HttpRequest], HttpResponse],
-    CustomSessionView.as_api_view(client=Client.BROWSER),
-)
+def get_custom_session_view() -> Callable[[HttpRequest], HttpResponse]:
+    # prevent circular import
+    from allauth.headless.constants import Client
+
+    return cast(
+        Callable[[HttpRequest], HttpResponse],
+        CustomSessionView.as_api_view(client=Client.BROWSER),
+    )
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", api_v1.urls),
     # django-allauth urls and headless apis
     path("accounts/", include("allauth.urls")),
-    path("_allauth/browser/v1/auth/session", custom_session_view),
+    path("_allauth/browser/v1/auth/session", get_custom_session_view()),
     path("_allauth/", include("allauth.headless.urls")),
 ]
 
