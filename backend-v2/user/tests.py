6d62c36e3bd9db5d487a7594d53bf406dd13311b
stabldev <114811070+stabldev@users.noreply.github.com>
2025-09-25 10:26:24 +0530

    test: add customuser model test cases

--- backend-v2/user/tests.py
+++ backend-v2/user/tests.py
@@ -1,3 +1,32 @@
+from typing import cast
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+from user.managers import CustomUserManager
+
+
+def get_user_manager():
+    """Helper function to get typed UserManager"""
+    user_manager = cast(CustomUserManager, get_user_model().objects)
+    return user_manager
+
+
+class UserModelTestCase(TestCase):
+    """Test cases for CustomUser model"""
+
+    def test_create_user(self):
+        user_manager = get_user_manager()
+        user = user_manager.create_user(email="user@user.com", password="user")
+        # run checks
+        self.assertEqual(user.email, "user@user.com")
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_super_user(self):
+        user_manager = get_user_manager()
+        admin_user = user_manager.create_superuser(
+            email="admin@admin.com", password="admin"
+        )
+        # run checks
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
