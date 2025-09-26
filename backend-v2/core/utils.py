16e3f61d1481d6b52602e9629ed180141e981d4a
stabldev <114811070+stabldev@users.noreply.github.com>
2025-09-26 10:52:17 +0530

    refactor: util function to get upload path

--- backend-v2/core/utils.py
+++ backend-v2/core/utils.py
@@ -1,35 +1,25 @@
 from typing import Any
 
 
-def get_avatar_upload_path(instance: Any, filename: str) -> str:
+def get_avatar_upload_path(instance: Any, _filename: str) -> str:
+    return get_upload_path(instance, "avatars")
+
+
+def get_banner_upload_path(instance: Any, _filename: str) -> str:
+    return get_upload_path(instance, "banners")
+
+
+def get_upload_path(instance: Any, folder: str) -> str:
     """
-    Helper function which returns upload path for avatars.
-    Checks if passed Instance is Profile or Quiblet model and adjust accordingly.
+    Generic helper function returning upload path.
+    Determines if instance is Profile or Quiblet based on attribute presence, then constructs path.
     """
-    if hasattr(instance, "username"):  # if this is a Profile model
-        indentifier = "u"
+    if hasattr(instance, "username"):  # Profile model
+        identifier = "u"
         unique_file_name = instance.username
-    else:
-        indentifier = "q"
+    else:  # Quiblet model
+        identifier = "q"
         unique_file_name = instance.name
 
-    extension = filename.split(".")[-1]
     extension = "webp"
-    return f"avatars/{indentifier}_{unique_file_name}.{extension}"
-
-
-def get_banner_upload_path(instance: Any, filename: str) -> str:
-    """
-    Helper function which returns upload path for banners.
-    Checks if passed Instance is Profile or Quiblet model and adjust accordingly.
-    """
-    if hasattr(instance, "username"):  # if this is a Profile model
-        indentifier = "u"
-        unique_file_name = instance.username
-    else:
-        indentifier = "q"
-        unique_file_name = instance.name
-
-    extension = filename.split(".")[-1]
-    extension = "webp"
-    return f"banners/{indentifier}_{unique_file_name}.{extension}"
+    return f"{folder}/{identifier}_{unique_file_name}.{extension}"
