43e456a162e5976d23ba75995025d81510bb5eb8
stabldev <114811070+stabldev@users.noreply.github.com>
2025-09-27 20:19:20 +0530

    style: add quiblets section on search-bar popover

--- frontend-v2/components/header/search-bar.tsx
+++ frontend-v2/components/header/search-bar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Search } from "lucide-react";
+import { ChevronRight, Search } from "lucide-react";
 import { useState } from "react";
 import { useDebouncedCallback } from "use-debounce";
 import {
@@ -7,6 +7,8 @@ import {
   PopoverAnchor,
   PopoverContent,
 } from "@/components/ui/popover";
+import { cn } from "@/lib/utils";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import IconInput from "../ui/icon-input";
 
 export default function SearchBar() {
@@ -31,8 +33,28 @@ export default function SearchBar() {
           onChange={debouncedOnChange}
         />
       </PopoverAnchor>
-      <PopoverContent className="w-[var(--radix-popover-trigger-width)]">
-        Place content for the popover here.
+      <PopoverContent className="flex w-[var(--radix-popover-trigger-width)] flex-col gap-2">
+        <span className="font-semibold text-muted-foreground text-sm">
+          Quiblets
+        </span>
+        <div className="group flex items-center gap-2">
+          <Avatar>
+            <AvatarImage src={""} alt="" />
+            <AvatarFallback>Q</AvatarFallback>
+          </Avatar>
+          <div className="flex flex-col">
+            <span className="font-medium text-sm">q/something</span>
+            <span className="text-muted-foreground text-sm/none">
+              1 member(s)
+            </span>
+          </div>
+          <ChevronRight
+            className={cn(
+              "ml-auto size-5 text-muted-foreground opacity-0 duration-250",
+              "group-hover:slide-in-from-left-25 group-hover:animate-in group-hover:opacity-100",
+            )}
+          />
+        </div>
       </PopoverContent>
     </Popover>
   );
