d4a63372ae5fc819f43494a88499058412f47fb1
stabldev <114811070+stabldev@users.noreply.github.com>
2025-10-04 10:22:33 +0530

    fix: trigger storage delete api on quib delete

--- backend-v2/quiblet/signals.py
+++ backend-v2/quiblet/signals.py
@@ -1,6 +1,5 @@
 # pyright: reportUnusedParameter=false
-import os
-from typing import Any, cast
+from typing import Any
 from django.db.models.signals import post_delete
 from django.dispatch import receiver
 
@@ -9,14 +8,7 @@ from .models import Quib
 
 @receiver(post_delete, sender=Quib)
 def delete_quib_cover_files(sender: Quib, instance: Quib, **kwargs: Any):
-    cover_path = getattr(cast(Any, instance.cover), "path", None)
-    if cover_path and isinstance(cover_path, str) and os.path.isfile(cover_path):
-        os.remove(cover_path)
-
-    cover_small_path = getattr(cast(Any, instance.cover_small), "path", None)
-    if (
-        cover_small_path
-        and isinstance(cover_small_path, str)
-        and os.path.isfile(cover_small_path)
-    ):
-        os.remove(cover_small_path)
+    """Post delete signal which deletes media files from storage."""
+    if instance.cover:
+        instance.cover.delete(save=False)
+        instance.cover_small.delete(save=False)
