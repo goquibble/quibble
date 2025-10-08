c730c133de9341b98c731ea3eeddd419c618233f
stabldev <114811070+stabldev@users.noreply.github.com>
2025-10-08 15:52:51 +0530

    chore: remove .client files

--- frontend-v2/components/feed.tsx
+++ frontend-v2/components/feed.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useQuery } from "@tanstack/react-query";
-import { getFeed } from "@/services/feed.client";
+import { getFeed } from "@/services/feed";
 import QuibCard from "./quib-card/quib-card";
 
 export default function Feed() {
