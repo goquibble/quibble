280e5e972036881bd35ffa2065159d26e0c43f32
stabldev <114811070+stabldev@users.noreply.github.com>
2025-10-10 23:12:42 +0530

    fix: layout auto expand issue and hide scrollbar

--- frontend-v2/components/layouts/quiblet-layout.tsx
+++ frontend-v2/components/layouts/quiblet-layout.tsx
@@ -4,8 +4,8 @@ export default function QuibletLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <div className="mx-auto flex max-w-300 flex-1">
-      <div className="flex flex-1 flex-col p-4 pr-2">{children}</div>
+    <div className="mx-auto flex min-w-0 max-w-300 flex-1">
+      <div className="flex min-w-0 flex-1 flex-col p-4 pr-2">{children}</div>
       <QuibletInfo />
     </div>
   );
--- frontend-v2/components/quiblet-highlights.tsx
+++ frontend-v2/components/quiblet-highlights.tsx
@@ -14,24 +14,30 @@ export default function QuibletHighlights() {
         <span className="mr-auto font-medium text-sm">Highlights</span>
         <ChevronUp className="size-4 text-muted-foreground transition-transform group-data-[state=closed]:rotate-180" />
       </CollapsibleTrigger>
-      <CollapsibleContent className="mt-2">
-        <div className="flex w-75 gap-2 rounded border p-2 hover:bg-input/30">
-          <Image
-            src="http://localhost:9000/quibble-media-dev-ap-south-1/covers/q-vcacj57-small.webp"
-            alt="cover-"
-            width={75}
-            height={75}
-            className="rounded-sm"
-          />
-          <div className="flex flex-col">
-            <span className="line-clamp-2 font-bold">
-              Which design is your favorite? Let me know below!
-            </span>
-            <span className="text-muted-foreground text-xs">
-              34 upvotes — 12 comments
-            </span>
+
+      <CollapsibleContent className="scrollbar-hide mt-2 flex gap-3 overflow-x-auto">
+        {Array.from({ length: 3 }).map((_, idx) => (
+          <div
+            key={idx.toString()}
+            className="flex w-75 flex-shrink-0 gap-2 rounded border p-2 hover:bg-input/30"
+          >
+            <Image
+              src="http://localhost:9000/quibble-media-dev-ap-south-1/covers/q-vcacj57-small.webp"
+              alt="cover"
+              width={75}
+              height={75}
+              className="rounded-sm"
+            />
+            <div className="flex flex-col">
+              <span className="line-clamp-2 font-bold">
+                Which design is your favorite? Let me know below!
+              </span>
+              <span className="text-muted-foreground text-xs">
+                34 upvotes — 12 comments
+              </span>
+            </div>
           </div>
-        </div>
+        ))}
       </CollapsibleContent>
     </Collapsible>
   );
