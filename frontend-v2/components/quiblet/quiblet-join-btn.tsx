52a2a8de81e3c3cd17f909b580304213532a5591
stabldev <114811070+stabldev@users.noreply.github.com>
2025-10-12 19:18:36 +0530

    feat: invalidate quiblet queries on join-or-leave

--- frontend-v2/components/quiblet/quiblet-join-btn.tsx
+++ frontend-v2/components/quiblet/quiblet-join-btn.tsx
@@ -1,4 +1,4 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
 import { joinOrLeaveQuiblet } from "@/services/quiblet";
 import { Button } from "../ui/button";
@@ -12,12 +12,17 @@ export default function QuibletJoinBtn({
   name,
   hasJoined,
 }: QuibletJoinBtnProps) {
+  const queryClient = useQueryClient();
   const [isJoined, setIsJoined] = useState(hasJoined);
 
   const mutation = useMutation({
     mutationFn: (action: "join" | "leave") => joinOrLeaveQuiblet(name, action),
     onSuccess: () => {
       setIsJoined((prev) => !prev);
+      queryClient.invalidateQueries({
+        queryKey: ["quiblet", name],
+        exact: true,
+      });
     },
   });
 
