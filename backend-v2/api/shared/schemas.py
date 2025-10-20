cf140ad04eb88812c66aa0bc4a91afd81fd6e810
stabldev <114811070+stabldev@users.noreply.github.com>
2025-10-20 17:42:25 +0530

    fix: handle vote schema validation errors

--- backend-v2/api/shared/schemas.py
+++ backend-v2/api/shared/schemas.py
@@ -12,6 +12,20 @@ class VoteSchema(Schema):
     user_vote_value: int | None
 
     @staticmethod
+    def resolve_upvotes(obj: Any) -> int:
+        """Resolve `upvotes` by checking if object has annotated attr or not."""
+        if hasattr(obj, "upvotes"):
+            return obj.upvotes
+        return obj.votes.filter(value=1).count()
+
+    @staticmethod
+    def resolve_downvotes(obj: Any) -> int:
+        """Resolve `downvotes` by checking if object has annotated attr or not."""
+        if hasattr(obj, "downvotes"):
+            return obj.downvotes
+        return obj.votes.filter(value=-1).count()
+
+    @staticmethod
     def resolve_user_vote_value(obj: Any) -> int | None:
         if hasattr(obj, "user_vote") and obj.user_vote:
             return obj.user_vote[0].value
