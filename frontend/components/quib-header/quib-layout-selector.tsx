4364862574c4c85ae0f3c85dfaa25445f99b9663
stabldev <114811070+stabldev@users.noreply.github.com>
2026-02-02 23:31:11 +0530

    fix: full-width quib layout selector

--- frontend/components/quib-header/quib-layout-selector.tsx
+++ frontend/components/quib-header/quib-layout-selector.tsx
@@ -16,21 +16,24 @@ export default function QuibLayoutSelector() {
       <DropdownMenuTrigger asChild>
         <Button size={"sm"} variant={"outline"}>
           {layout === "card" ? (
-            <Rows2 className="mr-2 h-4 w-4 text-primary" />
+            <Rows2 className="text-primary" />
           ) : (
-            <Rows3 className="mr-2 h-4 w-4 text-primary" />
+            <Rows3 className="text-primary" />
           )}
           <span className="capitalize">{layout}</span>
           <ChevronDown className="ml-2 h-4 w-4 text-muted-foreground" />
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
+      <DropdownMenuContent
+        align="end"
+        className="w-[var(--radix-dropdown-menu-trigger-width)]"
+      >
         <DropdownMenuItem onClick={() => setLayout("card")}>
-          <Rows2 className="mr-2 h-4 w-4" />
+          <Rows2 />
           Card
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setLayout("compact")}>
-          <Rows3 className="mr-2 h-4 w-4" />
+          <Rows3 />
           Compact
         </DropdownMenuItem>
       </DropdownMenuContent>
