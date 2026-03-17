a7226791a9ef0192a4d5bbcabf24a1f859ad8b42
stabldev <114811070+stabldev@users.noreply.github.com>
2026-03-17 16:59:44 +0530

    fix: nextjs build error by wrapping inside suspense

--- frontend/components/quib-header/index.tsx
+++ frontend/components/quib-header/index.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from "react";
 import QuibLayoutSelector from "./quib-layout-selector";
 import QuibSort from "./quib-sort";
 
 export default function QuibHeader() {
   return (
     <div className="flex items-center justify-between">
-      <QuibSort />
+      <Suspense>
+        <QuibSort />
+      </Suspense>
       <QuibLayoutSelector />
     </div>
   );
