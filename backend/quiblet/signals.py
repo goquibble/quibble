ece3451e400bf4736f69bd911a2a19318269ea67
stabldev <114811070+stabldev@users.noreply.github.com>
2026-03-18 10:02:34 +0530

    refactor: move moderation task to signal handler

--- backend/quiblet/signals.py
+++ backend/quiblet/signals.py
@@ -1,7 +1,9 @@
 # pyright: reportUnusedParameter=false
 from typing import Any
-from django.db.models.signals import post_delete
+from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
+
+from .tasks import moderate_quib
 
 from .models import Quib
 
@@ -12,3 +14,12 @@ def delete_quib_cover_files(sender: Quib, instance: Quib, **kwargs: Any):
     if instance.cover:
         instance.cover.delete(save=False)
         instance.cover_small.delete(save=False)
+
+
+@receiver(post_save, sender=Quib)
+def schedule_quib_moderation(
+    sender: type[Quib], instance: Quib, created: bool, **kwargs: Any
+):
+    """Schedule moderation task for new Quibs."""
+    if created:
+        moderate_quib.apply_async(args=[instance.id])
