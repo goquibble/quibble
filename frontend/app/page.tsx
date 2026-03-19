a644b6895cd74b3d1eacc55fa243b1a246725bbc
stabldev <114811070+stabldev@users.noreply.github.com>
2026-03-19 21:43:15 +0530

    fix: recent quibs proper overflow

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="mx-auto flex max-w-275 flex-1">
       <Feed />
       <div>
-        <div className="sticky top-14 flex h-[calc(100vh-var(--spacing)*14)] w-75 flex-col gap-4 p-4 pl-2">
+        <div className="scrollbar-track-rounded-full scrollbar-thin scrollbar-track-transparent scrollbar-thumb-accent scrollbar scrollbar-thumb-rounded-full sticky top-14 flex h-[calc(100vh-var(--spacing)*14)] w-75 flex-col gap-4 overflow-y-auto p-4 pl-2">
           <RecentQuibs />
           <LegalLinks />
         </div>
