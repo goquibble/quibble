538dde1ef7be6990a20e06e2ffac17ad681947fd
stabldev <114811070+stabldev@users.noreply.github.com>
2026-03-19 21:07:49 +0530

    fix: sidebars layouts

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,17 @@
 import Feed from "@/components/feed";
+import LegalLinks from "@/components/legal-links";
 import RecentQuibs from "@/components/recent-quibs";
 
 export default function Home() {
   return (
     <div className="mx-auto flex max-w-275 flex-1">
       <Feed />
-      <RecentQuibs />
+      <div>
+        <div className="sticky top-14 flex h-[calc(100vh-var(--spacing)*14)] w-75 flex-col gap-4 p-4 pl-2">
+          <RecentQuibs />
+          <LegalLinks />
+        </div>
+      </div>
     </div>
   );
 }
